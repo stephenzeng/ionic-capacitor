c7e4416757a856fba7695255148a62d61dffe28e
Stephen Zeng <Stephen.Zeng@cashconverters.com>
2019-10-18 13:12:35 +0800

    testing ci build

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  public name = 'foo';
 
   constructor() {}
+
+  ngOnInit() {
+    this.name = environment.name;
+  }
 
 }
